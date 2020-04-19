1a122457a9bb5a8c702bcc91056cde751a69a997
Bob Cassels <bobcassels@netscape.net>
2020-04-19 12:31:48 -0400

    Some changes Account for ROS2 changes Some personal preferences, such as gedit Assume this will be cloned into racecar_ws/base_ws/src

--- cron_jupyter.bash
+++ cron_jupyter.bash
@@ -1,3 +1,3 @@
 # adding to crontab -e
 # @reboot /home/racecar/racecar_ws/.scripts/cron_jupyter.bash
-bash -c /home/racecar/racecar_ws/.scripts/start_jupyter.bash
+bash -c /home/racecar/racecar_ws/base_ws/mn-scripts/start_jupyter.bash
--- start_jupyter.bash
+++ start_jupyter.bash
@@ -1,16 +1,3 @@
-source /home/racecar/.bashrc
-source /home/racecar/racecar_ws/.scripts/_racecars
-#source /opt/ros/melodic/setup.bash
-
-stdout=/home/racecar/racecar_ws/.scripts/cron_jupyter.stdout
-stderr=/home/racecar/racecar_ws/.scripts/cron_jupyter.stderr
-
-echo >> $stdout
-echo >> $stderr
-date >> $stdout
-date >> $stderr
-
-cd /home/racecar/racecar_ws/jupyter_ws
 
 # make sure wifi has connected
 wlan_cnt=$(nmcli | grep wlan0 | head -1 | grep ' connected' | wc -l)
@@ -19,4 +6,20 @@ while [ $wlan_cnt -ne 1 ]; do
     wlan_cnt=$(nmcli | grep wlan0 | head -1 | grep ' connected' | wc -l)
 done
 
-/usr/local/bin/jupyter-notebook >> $stdout 2>> $stderr &
+
+stdout=$RACECAR_SCRIPT_BASE/cron_jupyter.stdout
+stderr=$RACECAR_SCRIPT_BASE/cron_jupyter.stderr
+
+echo >> $stdout
+echo >> $stderr
+date >> $stdout
+date >> $stderr
+
+cd /home/racecar/racecar_ws/jupyter_ws
+
+source /home/racecar/.bashrc
+
+# Make Jupyter notebook terminals be bash
+export SHELL=/bin/bash
+
+jupyter-notebook >> $stdout 2>> $stderr &
