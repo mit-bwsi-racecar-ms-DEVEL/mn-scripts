657430f032128cf3d4243d22031699f53ffed142
Bob Cassels <bobcassels@netscape.net>
2020-04-19 23:22:17 -0400

    Wait for wifi, by default. As it was originally.

--- start_jupyter.bash
+++ start_jupyter.bash
@@ -1,15 +1,3 @@
-###
-#  When wifi is set up to connect, uncomment the code below
-###
-
-### wait for wifi to connect
-
-# wlan_cnt=$(nmcli | grep wlan0 | head -1 | grep ' connected' | wc -l)
-# while [ $wlan_cnt -ne 1 ]; do
-#     sleep 1
-#     wlan_cnt=$(nmcli | grep wlan0 | head -1 | grep ' connected' | wc -l)
-# done
-
 timestamp=`date -Iseconds`
 
 stdout=$RACECAR_SCRIPT_BASE/cron_jupyter.stdout
@@ -21,5 +9,14 @@ echo >> $stderr
 echo "$timestamp" >> $stderr
 
 cd /home/racecar/racecar_ws/jupyter_ws
+
+
+# wait for wifi to connect
+wlan_cnt=$(nmcli | grep wlan0 | head -1 | grep ' connected' | wc -l)
+while [ $wlan_cnt -ne 1 ]; do
+    sleep 1
+    wlan_cnt=$(nmcli | grep wlan0 | head -1 | grep ' connected' | wc -l)
+done
+
 
 jupyter-notebook >> $stdout 2>> $stderr &
