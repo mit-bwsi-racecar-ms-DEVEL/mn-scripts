5f1c8c775f053fdc1526924471ec10e77d558c48
emi-suzuki <emi.clairesuzuki@gmail.com>
2020-06-24 15:54:15 -0400

    A start script for jupyter.

---
+++ start_jupyter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /opt/ros/melodic/setup.bash
+source /home/racecar/racecar_ws/install/setup.bash
+/home/racecar/.local/bin/jupyter-notebook --config=/home/racecar/racecar_ws/base_ws/src/mn-scripts/jupyter_notebook_config.py
