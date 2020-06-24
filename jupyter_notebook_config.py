5a0dc633a656327610f3914d1b2c6195c6da7586
emi-suzuki <emi.clairesuzuki@gmail.com>
2020-06-24 16:31:19 -0400

    Updated the jupyter config to launch server in the /home/racecar/Documents/ directory.

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,5 +1,6 @@
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
+c.NotebookApp.notebook_dir = '/home/racecar/Documents/'
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.password = u'sha1:ebb1242beb89:92a3bb6f261902630730524f242b29fb80491d15'
