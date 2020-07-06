1004b79b3f1860e8d6cf9498d896159b9dd70217
emi-suzuki <emi.clairesuzuki@gmail.com>
2020-07-05 20:45:14 -0400

    Updated source.

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# When moving start.sh to a new location, racecar.service in /lib/systemd/system
+# must be updated with the correct path
+export ROS_IP=racecar
+export ROS_HOSTNAME=racecar
+export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
+
+source /opt/ros/eloquent/setup.bash
+source /home/racecar/racecar_ws/base_ws/install/setup.bash
+source /home/racecar/racecar_ws/base_ws/install/local_setup.bash
+
+ros2 launch racecar_mn teleop.launch.py
